d1bd7eb6e475e1d42e614c1b2141f468f154d0da
Philippe Timothe <PhilippeTimothe@gmail.com>
2025-06-18 20:29:34 -0400

    Fix vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "WhiteCoatLab", // Replace 'your-repo-name' with your actual GitHub repository name
+  base: "/WhiteCoatLab/", // Replace 'your-repo-name' with your actual GitHub repository name
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
