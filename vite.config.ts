9e91561eaf5b36d76460aa6cff87079c229377a4
Philippe Timothe <PhilippeTimothe@gmail.com>
2025-06-18 20:14:35 -0400

    vite config alteration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "WhiteCoatLab", // Replace 'your-repo-name' with your actual GitHub repository name
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
